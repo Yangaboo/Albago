ee7535a93eba1f1943381620519735fe2ef44b77
suyoung Ryu <suyoungryu@suyoungui-MBP.mshome.net>
2018-05-25 18:55:38 +0900

    시간자동화 구현

--- src/test/java/com/albago/webservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/albago/webservice/domain/PostsRepositoryTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -49,5 +51,23 @@ public class PostsRepositoryTest {
 //        assertThat(posts.getFavor(), is(1));
 //        assertThat(posts.getHate(), is(1));
         assertThat(posts.getCat_id(), is(1));
+    }
+
+    @Test
+    public void BaseTimeEntity_post () {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트")
+                .content("테스트")
+                .author("너너너")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
     }
 }
