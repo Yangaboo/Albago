bc69b8956190f0f76746dce0a4007b28e326355e
Junmo <yunedoopea610@gmail.com>
2018-05-27 11:56:24 +0900

    Initial setting

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+$primary-color: #333;
+
+body {
+  background-color: $primary-color;
+}
+</style>
