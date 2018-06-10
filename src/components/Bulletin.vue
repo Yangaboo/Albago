ac9d62ecbd62201d61411f7ac0909746f0e9e5fd
Junmo <yunedoopea610@gmail.com>
2018-06-10 12:21:41 +0900

    Add bulletin component, markup

---
+++ src/components/Bulletin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="bulletin">
+    <header class="bulletin__header">
+      <div class="bulletin__header__outline">
+        총 {{ totalBulletin }} / 금일 {{ todayBulletin }}
+      </div>
+      <div class="bulletin__header__navigation-tool">
+        <select class="bulletin__header__navigation-tool__category">
+          <option disabled selected>카테고리</option>
+        </select>
+        <input type="text" class="bulletin__header__navigation-tool__search-input">
+        <button class="bulletin__header__navigation-tool__search-button">검색</button>
+      </div>
+    </header>
+    <bulletin-list class="bulletin__bulletin-list"/>
+    <bulletin-paging class="bulletin__bulletin-paging"/>
+  </div>
+</template>
+
+<script>
+import BulletinList from './Bulletin/BulletinList';
+import BulletinPaging from './Bulletin/BulletinPaging';
+
+export default {
+  name: 'bulletin',
+  data() {
+
+  },
+  components: {
+    BulletinList,
+    BulletinPaging,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/setting';
+
+.bulletin {
+  height: 700px;
+  width: 100%;
+  background-image: linear-gradient(to left, #494f5c, #373c46);
+}
+
+</style>
---
+++ src/components/Bulletin/BulletinList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="bulletin-list">
+    <li class="bulletin-list__head">
+      <div class="bulletin-list__head__number">번호</div>
+      <div class="bulletin-list__head__title">제목</div>
+      <div class="bulletin-list__head__category">카테고리</div>
+      <div class="bulletin-list__head__date">작성일</div>
+      <div class="bulletin-list__head__numberOfGood">좋아요</div>
+      <div class="bulletin-list__head__numberOfBad">싫어요</div>
+    </li>
+    <li class="bulletin-list__bulletin-link">
+      <div class="bulletin-list__bulletin-link__number">{{ number }}</div>
+      <div class="bulletin-list__bulletin-link__title">{{ title }}</div>
+      <div class="bulletin-list__bulletin-link__category">{{ category }}</div>
+      <div class="bulletin-list__bulletin-link__date">{{ date }}</div>
+      <div class="bulletin-list__bulletin-link__numberOfGood">{{ numberOfGood }}</div>
+      <div class="bulletin-list__bulletin-link__numberOfBad">{{ numberOfBad }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'bulletin-list',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/setting';
+
+</style>
