ceb59b743b5602c5bae436124707c78dfa8483c4
Junmo <yunedoopea610@gmail.com>
2018-06-10 20:37:31 +0900

    Add bulletin list style

--- src/components/Bulletin.vue
+++ src/components/Bulletin.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="bulletin">
-    <header class="bulletin__header">
-      <div class="bulletin__header__outline">
-        총 {{ totalBulletin }}건 / 금일 {{ todayBulletin }}건
-      </div>
-      <div class="bulletin__header__navigation-tool">
-        <select class="bulletin__header__navigation-tool__category">
-          <option disabled selected>카테고리</option>
-        </select>
-        <input type="search"
-          placeholder="검색어.."
-          class="bulletin__header__navigation-tool__search-input">
-        <button class="bulletin__header__navigation-tool__search-button">검색</button>
-      </div>
-    </header>
-    <bulletin-list class="bulletin__bulletin-list"/>
-    <bulletin-paging class="bulletin__bulletin-paging"/>
+    <div class="bulletin__container">
+      <header class="bulletin__container__header">
+        <div class="bulletin__container__header__outline">
+          총 {{ totalBulletin }}건 / 금일 {{ todayBulletin }}건
+        </div>
+        <div class="bulletin__container__header__navigation-tool">
+          <select class="bulletin__container__header__navigation-tool__category">
+            <option disabled selected>카테고리</option>
+          </select>
+          <input type="search"
+            placeholder="검색어.."
+            class="bulletin__container__header__navigation-tool__search-input">
+          <button class="bulletin__container__header__navigation-tool__search-button">검색</button>
+        </div>
+      </header>
+      <bulletin-list class="bulletin__container__bulletin-list"/>
+      <bulletin-paging class="bulletin__container__bulletin-paging"/>
+    </div>
   </div>
 </template>
 
@@ -42,52 +44,53 @@ export default {
   height: 700px;
   width: 100%;
   background-image: linear-gradient(to left, #494f5c, #373c46);
-  padding-top: 56px;
   color: #fff;
-  @include e('header') {
-    $height: 30px;
-    width: 1200px;
-    height: $height;
+  @include e('container') {
     margin: 0 auto;
-    position: relative;
-    @include e('outline') {
-      font-size: 15px;
-      line-height: $height;
-    }
-    @include e('navigation-tool') {
-      position: absolute;
-      top: 0;
-      right: 0;
-      height: 100%;
-      font-size: 0;
-      display: flex;
-      %tool {
-        background-color: #fff;
+    width: 1200px;
+    padding-top: 56px;
+    @include e('header') {
+      $height: 30px;
+      height: $height;
+      position: relative;
+      @include e('outline') {
+        font-size: 15px;
+        line-height: $height;
+      }
+      @include e('navigation-tool') {
+        position: absolute;
+        top: 0;
+        right: 0;
         height: 100%;
-        font-size: 11px;
-        display: block;
-        box-sizing: border-box;
-        color: #494f5c;
-        border: none;
-        margin-left: 5px;
-      }
-      @include e('category') {
-        @extend %tool;
-        width: 75px;
-        appearance: none;
-        padding-left: 15px;
-      }
-      @include e('search-input') {
-        @extend %tool;
-        width: 175px;
-        padding-left: 35px;
-      }
-      @include e('search-button') {
-        @extend %tool;
-        width: 45px;
+        font-size: 0;
+        display: flex;
+        %tool {
+          background-color: #fff;
+          height: 100%;
+          font-size: 11px;
+          display: block;
+          box-sizing: border-box;
+          color: #494f5c;
+          border: none;
+          margin-left: 5px;
+        }
+        @include e('category') {
+          @extend %tool;
+          width: 75px;
+          appearance: none;
+          padding-left: 15px;
+        }
+        @include e('search-input') {
+          @extend %tool;
+          width: 175px;
+          padding-left: 35px;
+        }
+        @include e('search-button') {
+          @extend %tool;
+          width: 45px;
+        }
       }
     }
   }
 }
-
 </style>
--- src/components/Bulletin/BulletinList.vue
+++ src/components/Bulletin/BulletinList.vue
@@ -1,20 +1,32 @@
 <template>
   <ul class="bulletin-list">
     <li class="bulletin-list__head">
-      <div class="bulletin-list__head__number">번호</div>
-      <div class="bulletin-list__head__title">제목</div>
-      <div class="bulletin-list__head__category">카테고리</div>
-      <div class="bulletin-list__head__date">작성일</div>
-      <div class="bulletin-list__head__numberOfGood">좋아요</div>
-      <div class="bulletin-list__head__numberOfBad">싫어요</div>
+      <div class="bulletin-list__head__item bulletin-list__head__number">번호</div>
+      <div class="bulletin-list__head__item bulletin-list__head__title">제목</div>
+      <div class="bulletin-list__head__item bulletin-list__head__category">카테고리</div>
+      <div class="bulletin-list__head__item bulletin-list__head__date">작성일</div>
+      <div class="bulletin-list__head__item bulletin-list__head__numberOfGood">좋아요</div>
+      <div class="bulletin-list__head__item bulletin-list__head__numberOfBad">싫어요</div>
     </li>
-    <li class="bulletin-list__bulletin-link">
-      <div class="bulletin-list__bulletin-link__number">{{ number }}</div>
-      <div class="bulletin-list__bulletin-link__title">{{ title }}</div>
-      <div class="bulletin-list__bulletin-link__category">{{ category }}</div>
-      <div class="bulletin-list__bulletin-link__date">{{ date }}</div>
-      <div class="bulletin-list__bulletin-link__numberOfGood">{{ numberOfGood }}</div>
-      <div class="bulletin-list__bulletin-link__numberOfBad">{{ numberOfBad }}</div>
+    <li class="bulletin-list__link" v-for="bulletin in bulletins" :key="bulletin.number">
+      <div class="bulletin-list__link__item bulletin-list__link__number">
+        {{ bulletin.number }}
+      </div>
+      <div class="bulletin-list__link__item bulletin-list__link__title">
+        {{ bulletin.title }}
+      </div>
+      <div class="bulletin-list__link__item bulletin-list__link__category">
+        {{ bulletin.category }}
+      </div>
+      <div class="bulletin-list__link__item bulletin-list__link__date">
+        {{ bulletin.date }}
+      </div>
+      <div class="bulletin-list__link__item bulletin-list__link__numberOfGood">
+        {{ bulletin.numberOfGood }}
+      </div>
+      <div class="bulletin-list__link__item bulletin-list__link__numberOfBad">
+        {{ bulletin.numberOfBad }}
+      </div>
     </li>
   </ul>
 </template>
@@ -22,10 +34,69 @@
 <script>
 export default {
   name: 'bulletin-list',
+  data() {
+    return {
+      bulletins: [
+        {
+          number: 1,
+          title: '대덕소프트웨어마이스터고등학교 교사 아르바이트 후기',
+          category: '교육',
+          date: '2014-04-28',
+          numberOfGood: 100,
+          numberOfBad: 200,
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import '../../styles/setting';
 
+.bulletin-list {
+  margin: {
+    top: 10px;
+  }
+  border: {
+    top: 2px solid white;
+  }
+  %list-item {
+    width: 100%;
+    display: flex;
+  }
+  %flex-item {
+    font-size: 15px;
+    line-height: 19px;
+    height: 46px;
+    border-bottom: 1px solid #fff;
+    padding: {
+      top: 13px;
+      bottom: 13px;
+    }
+  }
+  @include e('head') {
+    @extend %list-item;
+    @include e('item') {
+      @extend %flex-item;
+      width: 10%;
+      text-align: center;
+    }
+    @include e('title') {
+      width: 50%;
+    }
+  }
+  @include e('link') {
+    @extend %list-item;
+    @include e('item') {
+      @extend %flex-item;
+      width: 10%;
+      text-align: center;
+    }
+    @include e('title') {
+      width: 50%;
+      text-align: left;
+    }
+  }
+}
 </style>
