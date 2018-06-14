ad3a68fa69631f3618f32d920690eb3e34247f11
Junmo <yunedoopea610@gmail.com>
2018-06-14 14:23:56 +0900

    Add set location name style

--- src/components/SetLocation.vue
+++ src/components/SetLocation.vue
@@ -3,14 +3,12 @@
     <div class="set-location">
       <div class="set-location__mask" @click="$emit('closeModal')"></div>
       <div class="set-location__modal">
-        <header class="set-location__modal__header">
-          <h2 class="set-location__modal__header__name">
-            위치 변경하기
-          </h2>
-          <button class="set-location__modal__header__close-button">
+        <h2 class="set-location__modal__name">
+          위치 변경하기
+          <button class="set-location__modal__name__close-button">
             &times;
           </button>
-        </header>
+        </h2>
         <div class="set-location__modal__main">
           <div class="set-location__modal__main__search">
             <input type="search" class="set-location__modal__main__search__input-text">
@@ -43,6 +41,9 @@ export default {
 
 <style lang="scss" scoped>
 @import '../styles/setting';
+$z-index-set-location: 1;
+$z-index-mask: 2;
+$z-index-modal: 3;
 
 .set-location {
   width: 100vw;
@@ -50,11 +51,60 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 1;
+  z-index: $z-index-set-location;
   @include e('mask') {
+    position: absolute;
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
+    z-index: $z-index-mask;
+  }
+  @include e('modal') {
+    $width-modal: 1100px;
+    $height-modal: 700px;
+    $radius: 5px;
+    $side-padding: 60px;
+    position: absolute;
+    z-index: $z-index-modal;
+    left: 50%;
+    top: 50%;
+    margin: {
+      left: -($width-modal / 2);
+      top: -($height-modal / 2);
+    }
+    width: $width-modal;
+    height: $height-modal;
+    background-color: #fff;
+    border-radius: $radius;
+    @include e('name') {
+      $padding-top: 30px;
+      width: 100%;
+      background-color: #494f5c;
+      height: 80px;
+      padding: {
+        top: $padding-top;
+        bottom: 20px;
+        left: $side-padding;
+        right: $side-padding;
+      }
+      border-top-left-radius: $radius;
+      border-top-right-radius: $radius;
+      font-size: 22px;
+      font-weight: bold;
+      line-height: 30px;
+      color: white;
+      @include e('close-button') {
+        position: absolute;
+        top: $padding-top;
+        right: $side-padding;
+        font-size: 30px;
+        font-weight: lighter;
+        line-height: inherit;
+        color: inherit;
+        background: none;
+        border: none;
+      }
+    }
   }
 }
 .modal {
