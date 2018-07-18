ec8b5ed2480098f5c1d1dc811f8437193bd9987a
Junmo <yunedoopea610@gmail.com>
2018-07-18 22:46:03 +0900

    Update constants

--- src/constants/category.js
+++ src/constants/category.js
@@ -1,5 +1,13 @@
 const category = {
-  CONVENIENCE_STORE: 0,
+  ALL: 0,
+  SERVE: 1,
+  MANAGE: 2,
+  PARK: 3,
+  IT: 4,
+  CLEAN: 5,
+  EXPRESS: 6,
+  OFFICE: 7,
+  ECT: 8,
 };
 Object.freeze(category);
 
