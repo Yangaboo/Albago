e251b4132468484fb5878720db83f1774fb22678
Junmo <yunedoopea610@gmail.com>
2018-07-18 22:46:48 +0900

    Start compare job

---
+++ src/components/CompareJob.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="compare-job">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'compare-job',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/setting';
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import MainContainer from '../components/MainContainer';
 import Bulletin from '../components/Bulletin';
+import CompareJob from '../components/CompareJob';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/bulletin',
       name: 'bulletin',
       component: Bulletin,
+    },
+    {
+      path: '/compare',
+      name: 'compare-job',
+      components: CompareJob,
     },
   ],
 });
