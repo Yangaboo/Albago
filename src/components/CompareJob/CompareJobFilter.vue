132eebe188d5b72d10ea94ddcc0d86e524cf0e2d
Junmo <yunedoopea610@gmail.com>
2018-08-27 22:43:21 +0900

    Refactor filter
    change priority list to component

--- src/components/CompareJob/CompareJobFilter.vue
+++ src/components/CompareJob/CompareJobFilter.vue
@@ -4,82 +4,39 @@
       <li><h3 class="section-name">우선순위로 비교</h3></li>
 
       <!-- 상관 없음이 선택되지 않은 항목, 비교 기준 -->
-      <li class="filter__priority-list__item"
+      <compare-job-priority
+        :is-relevant="true"
         v-for="(subject, index) in relevantSubjects"
-        :key="`sub1${index}`">
-        <div class="filter__priority-list__item__name">
-          {{ index + 1 }}. {{ subject.name }}
-        </div>
-        <div class="filter__priority-list__item__option">
-          <!-- 항목별 라디오 버튼 -->
-          <template v-for="option in subject.options">
-            <input class="filter__priority-list__item__option__radio"
-              type="radio"
-              :key="`radio-${option.value}`"
-              :id="option.value"
-              :value="option.value"
-              v-model="subject.value">
-            <label class="filter__priority-list__item__option__label"
-              :key="`label-${option.value}`"
-              :for="option.value">
-              {{ option.text }}
-            </label>
-          </template>
-
-          <!-- 상관 없음 라디오 버튼 -->
-          <input class="filter__priority-list__item__option__radio"
-            type="checkbox"
-            :id="`irrelevant-${subject.name}`"
-            v-model="subject.isIrrelevant"
-            @click="toIrrelevant(index)">
-          <label class="filter__priority-list__item__option__label"
-            :for="`irrelevant-${subject.name}`">
-            상관 없음
-          </label>
-        </div>
-      </li>
+        :key="`sub1${subject.name}`"
+        :index="index"
+        :name="subject.name"
+        :options="subject.options"
+        v-model="subject.value"
+        @toIrrelevant="toIrrelevant(index)">
+      </compare-job-priority>
 
       <!-- 상관 없음이 선택된 항목, 비교 기준에서 제외 -->
-      <li class="filter__priority-list__item filter__priority-list__item--irrelevant"
+      <compare-job-priority
+        :is-relevant="false"
         v-for="(subject, index) in irrelevantSubjects"
-        :key="`sub2${index}`">
-        <div class="filter__priority-list__item__name">
-          {{ subject.name }}
-        </div>
-        <div class="filter__priority-list__item__option">
-          <!-- 항목별 라디오 버튼 -->
-          <template v-for="option in subject.options">
-            <input class="filter__priority-list__item__option__radio"
-              type="radio"
-              :key="`radio-${option.value}`"
-              :id="option.value"
-              :value="option.value"
-              v-model="subject.value"
-              @click="toRelevant(index)">
-            <label class="filter__priority-list__item__option__label"
-              :key="`label-${option.value}`"
-              :for="option.value">
-              {{ option.text }}
-            </label>
-          </template>
-
-          <!-- 상관 없음 라디오 버튼 -->
-          <input class="filter__priority-list__item__option__radio"
-            type="checkbox"
-            :id="`irrelevant-${subject.name}`"
-            v-model="subject.isIrrelevant">
-          <label class="filter__priority-list__item__option__label"
-            :for="`irrelevant-${subject.name}`">
-            상관 없음
-          </label>
-        </div>
-      </li>
+        :key="`sub2${index}`"
+        :index="index"
+        :name="subject.name"
+        :options="subject.options"
+        v-model="subject.value"
+        @change="newValue => toRelevant(index, newValue)">
+      </compare-job-priority>
     </ul>
   </div>
 </template>
 
 <script>
+import CompareJobPriority from './CompareJobPriority';
+
 export default {
+  components: {
+    CompareJobPriority,
+  },
   name: 'compare-job-filter',
   data() {
     return {
@@ -91,7 +48,6 @@ export default {
             { text: '먼 거리', value: 'far' },
           ],
           value: 'close',
-          isIrrelevant: false,
         },
         {
           name: '시급',
@@ -100,7 +56,6 @@ export default {
             { text: '낮은 시급', value: 'low' },
           ],
           value: 'high',
-          isIrrelevant: false,
         },
         {
           name: '근무 기간',
@@ -109,7 +64,6 @@ export default {
             { text: '장기 근무', value: 'longDay' },
           ],
           value: 'shortDay',
-          isIrrelevant: false,
         },
         {
           name: '근무 시간',
@@ -118,7 +72,6 @@ export default {
             { text: '긴 시간', value: 'longTime' },
           ],
           value: 'shortTime',
-          isIrrelevant: false,
         },
       ],
       irrelevantSubjects: [],
@@ -129,8 +82,8 @@ export default {
       this.relevantSubjects[index].value = '';
       this.irrelevantSubjects.push(...this.relevantSubjects.splice(index, 1));
     },
-    toRelevant(index) {
-      this.irrelevantSubjects[index].isIrrelevant = false;
+    toRelevant(index, value) {
+      this.irrelevantSubjects[index].value = value;
       this.relevantSubjects.push(...this.irrelevantSubjects.splice(index, 1));
     },
   },
@@ -146,63 +99,6 @@ $point-color1: #ffe886;
 .filter {
   box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.16);
   position: relative;
-  @include e('priority-list') {
-    @include e('item') {
-      $item-height: 80px;
-      $item-content-height: 35px;
-      background-color: $main-color2;
-      height: $item-height;
-      display: flex;
-      border-bottom: 1px solid $point-color1;
-      color: #fff; // name color
-      padding: {
-        top: ($item-height - $item-content-height) / 2;
-        left: 40px;
-      }
-      &:last-child {
-        border-bottom: none;
-      }
-      @include m('irrelevant') {
-        background-color: $main-color2 / 1.2;
-        color: #aaa;
-        &:first-child {
-          margin-top: 100px;
-        }
-      }
-      @include e('name') {
-        width: 30%;
-        height: $item-content-height;
-        font-size: 20px;
-        line-height: $item-content-height;
-        color: inherit;
-      }
-      @include e('option') {
-        width: 60%;
-        display: flex;
-        @include e('radio') {
-          display: none;
-          &:checked + label {
-            background-color: $main-color1;
-            color: #fff;
-            box-shadow: inset 0 2px 2px 0 rgba(0, 0, 0, 0.16);
-          }
-        }
-        @include e('label') {
-          flex: 1;
-          height: $item-content-height;
-          border-radius: 2px;
-          background-color: #fff;
-          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16);
-          display: block;
-          text-align: center;
-          line-height: $item-content-height;
-          font-size: 16px;
-          color: $main-color1;
-          margin-left: 10px;
-        }
-      }
-    }
-  }
 }
 
 </style>
---
+++ src/components/CompareJob/CompareJobPriority.vue
@@ -0,0 +1,129 @@
+<template>
+  <li class="subject" :class="{'subject--irrelevant': !isRelevant}">
+    <div class="subject__name">
+      <span v-show="isRelevant">{{ index + 1 }}.</span> {{ name }}
+    </div>
+    <div class="subject__option">
+      <!-- 항목별 라디오 버튼 -->
+      <template v-for="option in options">
+        <input
+          class="subject__option__radio"
+          type="radio"
+          :key="`radio-${option.value}`"
+          :id="option.value"
+          :value="option.value"
+          v-model="mutableValue"
+          @change="$emit('change', mutableValue)">
+        <label
+          class="subject__option__label"
+          :key="`label-${option.value}`"
+          :for="option.value">
+          {{ option.text }}
+        </label>
+      </template>
+
+      <!-- 상관 없음 라디오 버튼 -->
+      <input
+        class="subject__option__radio"
+        type="radio"
+        :id="`irrelevant-${name}`"
+        :checked="!isRelevant"
+        @click="$emit('toIrrelevant')">
+      <label class="subject__option__label"
+        :for="`irrelevant-${name}`">
+        상관 없음
+      </label>
+    </div>
+    <!-- <div class="subject__order-btn-cover">
+      <button class="order-btn forward"></button>
+      <button class="order-btn backward"></button>
+    </div> -->
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'compare-job-priority',
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+  props: {
+    index: { type: Number },
+    name: { type: String },
+    options: { type: Array },
+    value: { type: String },
+    irrelevantValue: { type: Boolean },
+    isRelevant: { type: Boolean },
+  },
+  data() {
+    return {
+      mutableValue: this.value,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/setting';
+$main-color1: #494f5c;
+$main-color2: #636d81;
+$point-color1: #ffe886;
+
+.subject {
+  $item-height: 80px;
+  $item-content-height: 35px;
+
+  background-color: $main-color2;
+  height: $item-height;
+  display: flex;
+  border-bottom: 1px solid $point-color1;
+  color: #fff; // name color
+  padding: {
+    top: ($item-height - $item-content-height) / 2;
+    left: 40px;
+  }
+  &:last-child {
+    border-bottom: none;
+  }
+  @include m('irrelevant') {
+    background-color: $main-color2 / 1.2;
+    color: #aaa;
+    &:first-child {
+      margin-top: 100px;
+    }
+  }
+  @include e('name') {
+    width: 30%;
+    height: $item-content-height;
+    font-size: 20px;
+    line-height: $item-content-height;
+    color: inherit;
+  }
+  @include e('option') {
+    width: 60%;
+    display: flex;
+    @include e('radio') {
+      display: none;
+      &:checked + label {
+        background-color: $main-color1;
+        color: #fff;
+        box-shadow: inset 0 2px 2px 0 rgba(0, 0, 0, 0.16);
+      }
+    }
+    @include e('label') {
+      flex: 1;
+      height: $item-content-height;
+      border-radius: 2px;
+      background-color: #fff;
+      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16);
+      display: block;
+      text-align: center;
+      line-height: $item-content-height;
+      font-size: 16px;
+      color: $main-color1;
+      margin-left: 10px;
+    }
+  }
+  }
+</style>
