d015a30cb9190e9403267ddc51bd31509a3b5ebd
Junmo <yunedoopea610@gmail.com>
2018-09-03 21:35:16 +0900

    Add dummy data job list

--- src/components/CompareJob/CompareJobList.vue
+++ src/components/CompareJob/CompareJobList.vue
@@ -5,6 +5,17 @@
 <script>
 export default {
   name: 'compare-job-list',
+  data() {
+    return {
+      jobLists: [{
+        name: 'GS25 편의점 아르바이트',
+        distance: '16km',
+        period: '17일 근무 (2018.04.01 ~ 2018.04.18)',
+        workTime: '5시간 (17시 ~ 22시)',
+        hourlyWage: '8,690원',
+      }],
+    };
+  },
 };
 </script>
 
