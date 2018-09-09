c03ec16e245bcdeba5bc92a74c6c17a8cca12c08
Nam-kyu <skarb7746@gmail.com>
2018-09-09 23:56:57 +0900

    crawling

---
+++ src/main/java/com/albago/webservice/URL.java
@@ -0,0 +1,13 @@
+package com.albago.webservice;
+
+public class URL {
+    private String url;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
--- src/main/java/com/albago/webservice/web/WebRestController.java
+++ src/main/java/com/albago/webservice/web/WebRestController.java
@@ -1,13 +1,22 @@
 package com.albago.webservice.web;
 
-import com.albago.webservice.domain.CommentsRepository;
+import com.albago.webservice.URL;
 import com.albago.webservice.dto.posts.CommentsSaveRequestDto;
 import com.albago.webservice.dto.posts.PostsSaveRequestDto;
 import com.albago.webservice.service.CommentsService;
 import com.albago.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.json.JSONObject;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Document;
+
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 @RestController
 @AllArgsConstructor
@@ -16,7 +25,7 @@ public class WebRestController {
     private CommentsService commentsService;
 
     @GetMapping("/")
-    public void hello() {
+    void hello() {
         return;
     }
 
@@ -30,9 +39,8 @@ public class WebRestController {
         return commentsService.save(dto);
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/posts/{post_id}")
     public String getPosts(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 
@@ -43,7 +51,38 @@ public class WebRestController {
     }
 
     @PostMapping("/filter")
-    public void filter() {
-        return;
+    public HashMap<String, String> filter(@RequestBody URL url) throws IOException {
+        HashMap<String, String> res = new HashMap<String, String>();
+//        JSONObject json = new JSONObject(res);
+        String uri;
+        uri = url.getUrl();
+        Connection.Response response = Jsoup.connect(uri)
+                .method(Connection.Method.GET)
+                .execute();
+        Document albaDocument = response.parse();
+        Element address = albaDocument.select("li[class=address divide]").select("strong").first();
+        Element title = albaDocument.select("p[class=detailTitle]").first();
+        Element workTime = albaDocument.select("li[class=worktimecd]").first();
+        Element period = albaDocument.select("li[class=workperiodcd]").select("a").first();
+        Element pay = albaDocument.select("p[class=pay]").first();
+        Element workDate = albaDocument.select("li[class=workweekcd]").select("a").first();
+        Element age = albaDocument.select("li[class=agetemp]").first();
+
+        String addressValue = address.text();
+        String titleValue = title.text();
+        String workTimeValue = workTime.text();
+        String periodValue = period.text();
+        String payValue = pay.text();
+        String workDateValue = workDate.text();
+        String ageValue = age.text();
+
+        res.put("name", titleValue);
+        res.put("workTime", workTimeValue);
+        res.put("period", periodValue);
+        res.put("hourlyWage", payValue);
+        res.put("days", workDateValue);
+        res.put("age", ageValue);
+
+        return res;
     }
 }
