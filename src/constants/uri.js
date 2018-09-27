7b209cd1b4afe73f819deda59c276302c64276a4
Junmo <yunedoopea610@gmail.com>
2018-09-27 22:35:29 +0900

    Add axios, uri constants

---
+++ src/constants/uri.js
@@ -0,0 +1,2 @@
+const uri = 'http://54.180.76.132:8080';
+export default uri;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import axios from 'axios';
 import App from './App';
 import router from './router';
 
 Vue.prototype.$EventBus = new Vue();
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
