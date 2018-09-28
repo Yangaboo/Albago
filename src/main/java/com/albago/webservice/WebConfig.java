3b842cc965802f2ba8f268807cce337cbc799656
Nam-kyu <skarb7746@gmail.com>
2018-09-28 09:07:32 +0900

    Add data

--- src/main/java/com/albago/webservice/WebConfig.java
+++ src/main/java/com/albago/webservice/WebConfig.java
@@ -1,8 +1,12 @@
 package com.albago.webservice;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
@@ -16,4 +20,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                 .allowedOrigins("*")
                 .maxAge(3600);
     }
+
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+//        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+//        resolver.setOneIndexedParameters(true);
+//        argumentResolvers.add(resolver);
+//        super.addArgumentResolvers(argumentResolvers);
+//    }
 }
